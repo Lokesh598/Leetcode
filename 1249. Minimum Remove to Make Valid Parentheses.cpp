762a3927b5218cb92d0ec343398fc59116786086
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2022-03-15 09:10:16 +0530

    Create 1249. Minimum Remove to Make Valid Parentheses.cpp

---
+++ 1249. Minimum Remove to Make Valid Parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> st;
+        string res = "";
+        
+        for(int i = 0; i< s.length(); i++) {
+            if(s[i] == '(') {
+                st.push(i);
+            } else if(s[i] == ')') {
+                if(st.empty()) 
+                    s[i] = '#';
+                else
+                    st.pop();
+            }
+        }
+        // )) == ##
+        //if top == # 
+        //pop()
+        while(!st.empty()) {
+            s[st.top()] = '#';
+            st.pop();
+        }
+        
+        for(int i = 0; i<s.length(); i++) {
+            if(s[i] != '#')
+                res.push_back(s[i]);
+        }
+        return res;
+    }
+};
