af29e29894f9a936db1c86527eff528ae394e107
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2022-03-24 07:32:18 +0530

    leetcode march day 24

---
+++ 0881. Boats to Save People.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int ans = 0;
+        sort(people.begin(), people.end());
+        int i = 0;
+        int j = people.size()-1;
+        while(i<=j) {
+            ans++;
+            if(people[i]+people[j]<=limit)
+                i++;
+            j--;
+        }
+        return ans;
+    }
+};
