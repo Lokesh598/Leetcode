95cc8fda571842ab9cc3836d107e3e9528b000b4
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2022-04-04 19:52:48 +0530

    updated with 2nd solution

--- 0704. Binary Search.cpp
+++ 0704. Binary Search.cpp
@@ -25,3 +25,34 @@ public:
         return -1;
     }
 };
+
+2nd method
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        
+        int left = 0, right = n-1;
+        
+        int mid;
+        
+        while(left <= right) {
+            mid = left + (right - left) / 2;
+            
+            if(nums[mid] == target) {
+                return mid;
+            }
+                //go left part
+            if(nums[mid] > target) {
+                right = mid-1;
+            }
+            else {
+                left = mid+1;
+            }
+            
+        }
+        return -1;
+        
+    }
+};
