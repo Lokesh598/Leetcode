7fb6f62b0d41a5380641182487a0ee2e73a98e2a
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2022-04-06 10:12:37 +0530

    easy || binary search

---
+++ 0367. Valid Perfect Square.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int low = 1;
+        long high = num;
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
+                low = (int)mid + 1;
+            } else {
+                high = (int)mid - 1;
+            }
+        }
+        return false;
+    }
+};
