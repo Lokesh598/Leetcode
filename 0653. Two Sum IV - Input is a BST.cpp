96aebe08b201801807636b60de5d9e74e364554e
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2022-04-19 07:55:26 +0530

    Update 0653. Two Sum IV - Input is a BST.cpp

--- 0653. Two Sum IV - Input is a BST.cpp
+++ 0653. Two Sum IV - Input is a BST.cpp
@@ -16,3 +16,21 @@ class Solution {
         inorder(root->right, nums);
     }
 };
+
+
+this problem solved using another method with the help of hash table
+
+class Solution {
+public:
+   bool findTarget(TreeNode* root, int k) {
+        unordered_set<int> set;
+        return dfs(root, set, k);
+    }
+    
+    bool dfs(TreeNode* root, unordered_set<int>& set, int k){
+        if(root == NULL)return false;
+        if(set.count(k - root->val))return true;
+        set.insert(root->val);
+        return dfs(root->left, set, k) || dfs(root->right, set, k);
+    }
+};
