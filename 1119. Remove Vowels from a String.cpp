8a3412bf863dd344ecc18c5df576dae7e881c34a
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2022-04-22 18:50:56 +0530

    easy || string

--- 1119. Remove Vowels from a String.cpp
+++ 1119. Remove Vowels from a String.cpp
@@ -15,3 +15,22 @@ public:
 	    return s.substr(0, j);
 	}
 };
+
+if we are allowed to use extra space
+
+string remVowel(string str)
+{
+    vector<char> vowels = {'a', 'e', 'i', 'o', 'u',
+                           'A', 'E', 'I', 'O', 'U'};
+     
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (find(vowels.begin(), vowels.end(),
+                      str[i]) != vowels.end())
+        {
+            str = str.replace(i, 1, "");
+            i -= 1;
+        }
+    }
+    return str;
+}
