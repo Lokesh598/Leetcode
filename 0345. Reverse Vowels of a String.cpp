952ac519deca3ff7752ecf93f185741299adbc5c
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2022-04-22 18:30:26 +0530

    easy || string, two pointers

---
+++ 0345. Reverse Vowels of a String.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int dict[256] = {0};
+        dict['a'] = 1, dict['A'] = 1;
+        dict['e'] = 1, dict['E'] = 1;
+        dict['i'] = 1, dict['I'] = 1;
+        dict['o'] = 1, dict['O'] = 1;
+        dict['u'] = 1, dict['U'] = 1;
+        int start = 0, end = (int)s.size() - 1;
+        while(start < end){
+            while(start < end && dict[s[start]] == 0) start++;
+            while(start < end && dict[s[end]] == 0) end--;
+            swap(s[start],s[end]);
+            start++;end--;
+        }
+        return s;
+    }
+};
+
+
+  class Solution {
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            i = s.find_first_of("aeiouAEIOU", i);
+            j = s.find_last_of("aeiouAEIOU", j);
+            if (i < j) {
+                swap(s[i++], s[j--]);
+            }
+        }
+        return s;
+  }
